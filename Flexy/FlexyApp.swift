5f8d2362bd5801d40aece9084514d57e3798ba51
Henil <henilp467@gmail.com>
2023-11-15 14:23:57 +1100

    Initial Commit

---
+++ Flexy/FlexyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlexyApp.swift
+//  Flexy
+//
+//  Created by Henil Patel on 15/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct FlexyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
