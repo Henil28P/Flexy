f0ae14022e1f8f342a40bf895fb7fec8c6b82a3c
Henil <henilp467@gmail.com>
2023-11-17 18:50:16 +1100

    Start page rough design

--- Flexy/View/ViewA.swift
+++ Flexy/View/ViewA.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct ViewA: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.green
+            
+            Text("Flexy")
+                .foregroundColor(Color.white)
+                .font(.system(size: 100.0))
+        }
     }
 }
 
