04a0ff7504f8be696db330170e1f25f5e8efdb05
estellecatelan <estelle@catelangroup.com.au>
2023-11-20 13:09:08 +1100

    First home page

--- Flexy/View/ViewC.swift
+++ Flexy/View/ViewC.swift
@@ -9,10 +9,39 @@ import SwiftUI
 
 struct ViewC: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.init(red: 232 / 255, green: 244 / 255, blue: 220 / 255)
+                .ignoresSafeArea()
+            
+            VStack {
+                Text("Let's Stretch")
+                    .font(.title)
+                Text("please select a body part to get started")
+                Image("HomeScreenMonkey")
+                    .resizable()
+                    .scaledToFit()
+                    .cornerRadius(15)
+                Spacer()
+                                
+                                Button(action: {
+                                    // Action to be performed when the button is tapped
+                                    // You can navigate to another view or perform some other action here
+                                    print("Button tapped!")
+                                }) {
+                                    Text("Explore all exercises")
+                                        .foregroundColor(.white)
+                                        .padding()
+                                        .background(Color.green)
+                                        .cornerRadius(20)
+                                }
+                
+            }
+            
+        }
+       
+        
     }
 }
-
 #Preview {
     ViewC()
 }
