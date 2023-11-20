a4c6fef19867d9b4591d52b1ab4361b255a29a77
Si Thu <39857496+SiThuzzz@users.noreply.github.com>
2023-11-20 14:29:54 +1100

    list view cards

---
+++ Flexy/View/ExerciseListView.swift
@@ -0,0 +1,18 @@
+//
+//  ExerciseListView.swift
+//  Flexy
+//
+//  Created by Si Thu on 20/11/2023.
+//
+
+import SwiftUI
+
+struct ExerciseListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExerciseListView()
+}
---
+++ Flexy/View/LoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingView.swift
+//  Flexy
+//
+//  Created by Si Thu on 20/11/2023.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
