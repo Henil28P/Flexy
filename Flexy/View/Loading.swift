056c10dadecea7ec5db357563036014ca22afddf
estellecatelan <estelle@catelangroup.com.au>
2023-11-22 13:21:20 +1100

    Loading page created. Background made to white for Feedback (video) page.

---
+++ Flexy/View/Loading.swift
@@ -0,0 +1,18 @@
+//
+//  Loading.swift
+//  Flexy
+//
+//  Created by Estelle Catelan on 22/11/2023.
+//
+
+import SwiftUI
+
+struct Loading: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Loading()
+}
