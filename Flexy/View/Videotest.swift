d4258a9813fc8fb2c7d53a42797418e926269e70
Henil <henilp467@gmail.com>
2023-11-23 11:51:35 +1100

    Loading screen updated and linked. Video works again.

---
+++ Flexy/View/Videotest.swift
@@ -0,0 +1,18 @@
+//
+//  Videotest.swift
+//  Flexy
+//
+//  Created by Henil Patel on 23/11/2023.
+//
+
+import SwiftUI
+
+struct Videotest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Videotest()
+}
