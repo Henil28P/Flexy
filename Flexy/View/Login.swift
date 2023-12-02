7ccebd87722aecf7079f6dd91f284ea454457d67
Henil <henilp467@gmail.com>
2023-12-02 17:23:04 +1100

    Login SwiftUI view page created. (Only UI made, no functionality).

--- Flexy/View/Login.swift
+++ Flexy/View/Login.swift
@@ -8,8 +8,53 @@
 import SwiftUI
 
 struct Login: View {
+    @State private var username = ""
+    @State private var password = ""
+    @State private var wrongUsername = ""
+    @State private var wrongPassword = ""
+    @State private var showingLoginScreen = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ZStack {
+                Color.green
+                    .ignoresSafeArea()
+                Circle()
+                    .scale(1.7)
+                    .foregroundColor(.white.opacity(0.15))
+                Circle()
+                    .scale(1.35)
+                    .foregroundColor(.white)
+                
+                VStack {
+                    Text("Login")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding()
+                    TextField("Username", text: $username)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        // .border(.red, width: CGFloat(wrongUsername))
+                    
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        // .border(.red, width: CGFloat(wrongPassword))
+                    
+                    Button("Login") {
+                        // Authenticate user
+                    }
+                    .foregroundColor(.white)
+                    .frame(width: 300, height: 50)
+                    .background(Color.green)
+                    .cornerRadius(10)
+                }
+            }
+        } .navigationBarHidden(true)
     }
 }
 
