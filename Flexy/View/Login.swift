5ff76792aba61a4df5dd6731cc314f1ec96e7af7
Henil <henilp467@gmail.com>
2023-12-02 16:53:55 +1100

    Created Login SwiftUI view page.

---
+++ Flexy/View/Login.swift
@@ -0,0 +1,18 @@
+//
+//  Login.swift
+//  Flexy
+//
+//  Created by Henil Patel on 2/12/2023.
+//
+
+import SwiftUI
+
+struct Login: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Login()
+}
